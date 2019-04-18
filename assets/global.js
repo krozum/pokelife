9beef68d2a29eac46fcac079ac435de00f6e58d5
krozum <34769139+krozum@users.noreply.github.com>
2019-04-18 17:36:29 +0200

    Create global.js

---
+++ assets/global.js
@@ -0,0 +1,211 @@
+.triangle-isosceles {
+    position: relative;
+    padding: 15px;
+    margin: 1em 0 3em;
+    color: #000;
+    background: #4caf50;
+    border-radius: 10px;
+    box-shadow: 2px 3px 1px 0px #b8b8b8;
+}
+
+.triangle-isosceles:after {
+    content: "";
+    position: absolute;
+    bottom: -15px;
+    right: 17px;
+    border-width: 15px 15px 0;
+    border-style: solid;
+    border-color: #4caf50 transparent;
+    display: block;
+    width: 0;
+}
+
+.icon-select {
+    width: 0px;
+}
+.icon-select .selected-box {
+    position: relative;
+    margin: 0px;
+    padding: 0px;
+}
+.icon-select .selected-box img {
+    position: relative;
+    margin: 0px;
+    padding: 0px;
+    border-radius: 4px;
+}
+.icon-select .selected-icon {
+    position: absolute;
+    margin: 0px;
+    padding: 0px;
+    top: 5px;
+    left: 5px;
+    width: 48px;
+    height: 48px;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+}
+.icon-select .component-icon {
+    position: absolute;
+    bottom: 5px;
+    right: 4px;
+    display: none;
+}
+.icon-select .box {
+    position: absolute;
+    top: 70px;
+    left: 0px;
+    margin: 0px;
+    padding: 0px;
+    width: 168px;
+    padding-bottom: 5px;
+    border: 2px solid #999999;
+    background-color: #EEEEEE;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+    overflow: auto;
+}
+.icon-select .icon {
+    position: relative;
+    margin: 5px 0px 0px 5px;
+    padding: 0px;
+    width: 48px;
+    height: 48px;
+    border: 1px solid #CCCCCC;
+    background-color: #FFFFFF;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+    overflow: hidden;
+    float: left;
+}
+.icon-select .icon:hover {
+    border: 1px solid #000000;
+}
+.icon-select .icon.selected {
+    position: relative;
+    margin: 5px 0px 0px 5px;
+    padding: 0px;
+    width: 48px;
+    height: 48px;
+    border: 1px solid #EEEEEE;
+    background-color: #EEEEEE;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+    overflow: hidden;
+    float: left;
+}
+.btn-druzyna {
+    width: 22px;
+    padding: 0px;
+    margin: 2px;
+}
+
+.list-group-item-info {
+    color: #3c3c3c;
+    background-color: #eaeaea;
+}
+
+
+.movecategory_physical {
+	background-color: #C92112;
+	border-color:#d96358;
+}
+.movecategory_physical:disabled {
+	background-color: #E94132;
+}
+
+.movecategory_special {
+	background-color: #4F5870;
+	border-color: #333948;
+}
+.movecategory_special:disabled {
+	background-color: #6F7890;
+}
+.movecategory_status {
+	background-color: #8C888C;
+	border-color: #5b585b;
+}
+.movecategory_status:disabled {
+	background-color: #ACA8AC;
+}
+.sortable-btn-up, .sortable-btn-down {
+    margin-top: -4px;
+    margin-left: 5px;
+}
+
+div:not(#plecak-tm) > div >.thumbnail-plecak {
+    background-color: white;	
+}
+a {
+    color: #4c4c4c !important;	
+}	
+
+.nav a {
+    color: white !important;
+}
+
+.nav-tabs a {
+    color: #4c4c4c !important;	
+}
+#shoutbox-panel {
+    background-color: #d4cbca;
+}
+
+#shouts ul li, #shouts {
+    background-color: rgb(234, 234, 234);
+}
+.nav-tabs>li.active>a, .nav-tabs>li.active>a:focus, .nav-tabs>li.active>a:hover {
+    color: #555;
+    background-color: #eaeaea;
+}
+	
+.panel-primary, .panel-primary>.panel-heading, .awatar, h2, .modal-header {	
+    border-color: #74b5b1 !important;
+}
+	
+.panel-primary>.panel-body, .osiagniecie {	
+   background-color: #ffffff;	
+}
+
+.plugin-button {
+    border: none !important;	
+}
+
+#menu, #pasek_skrotow {
+    background-color: #272727;
+    color: #f0f0f0;
+}
+
+#menu li, #menu ul {
+    background-color: #272727;
+}
+
+#menu a {
+    background-color: #272727;
+}
+#pokemony-druzyna td {
+    border: none !important;	
+}
+#sidebar table.table-striped.table-condensed td {
+    vertical-align: bottom !important;    
+    padding-bottom: 7px !important;
+}
+
+
+#sidebar table.table-striped.table-condensed td:last-child div {
+    padding: 5px;
+    padding-bottom: 6px;
+}
+
+#fastShop button.confirm {
+    background-color: #d9534f !important;
+    border-color: #d9534f !important;
+}
+
+#fastShop button {
+    color: #333 !important;	
+}
