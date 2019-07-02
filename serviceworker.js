44330d412f5feb4ae1ffa01f594c38fbdb5d906b
krozum <34769139+krozum@users.noreply.github.com>
2019-07-02 10:53:07 +0200

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,17 +1,1 @@
-self.addEventListener('push', function(e) {
-  var options = {
-    body: 'This notification was generated from a push!',
-    vibrate: [100, 50, 100],
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: '2'
-    },
-    actions: [
-      {action: 'explore', title: 'Explore this new world'},
-      {action: 'close', title: 'Close'}
-    ]
-  };
-  e.waitUntil(
-    self.registration.showNotification('Hello world!', options)
-  );
-});
+setTimeout(postMessage(100),2000);
