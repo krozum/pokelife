6e963d28380aafc64b757b21746de5152ab94012
krozum <34769139+krozum@users.noreply.github.com>
2019-08-24 15:16:23 +0200

    Update FightSimulator.user.js

--- FightSimulator.user.js
+++ FightSimulator.user.js
@@ -8,6 +8,21 @@
 // @updateURL    https://github.com/krozum/pokelife/raw/master/FightSimulator.user.js
 // ==/UserScript==
 
+let poziom_pokemona = 100;
+let krytyczny_cios = 1;
+let punkty_ataku_pokemona = 2610;
+let stała_ataku = 60;
+let punkty_obrony_pokemona = 928;
+let modyfikator = 1;
+let stab = 1.3;
+let losowa_liczba = Math.floor((Math.random() * 50) + 205);
 
-var dmg = 1;
-console.log(dmg);
+
+var dmg =  ((((poziom_pokemona * 0.4 * krytyczny_cios) + 2) * punkty_ataku_pokemona * stała_ataku / 50 / punkty_obrony_pokemona ) + 2) * modyfikator * stab * (205 / 255) * 1.5 * 5;
+console.log("min: " + dmg);
+
+dmg =  ((((poziom_pokemona * 0.4 * krytyczny_cios) + 2) * punkty_ataku_pokemona * stała_ataku / 50 / punkty_obrony_pokemona ) + 2) * modyfikator * stab * (losowa_liczba / 255) * 1.5 * 5;
+console.log("random: " +dmg);
+
+dmg =  ((((poziom_pokemona * 0.4 * krytyczny_cios) + 2) * punkty_ataku_pokemona * stała_ataku / 50 / punkty_obrony_pokemona ) + 2) * modyfikator * stab * (255 / 255) * 1.5 * 5;
+console.log("max: " +dmg);
