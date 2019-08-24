3fc91830a71cefdfba2f08e548957c13b33af898
krozum <34769139+krozum@users.noreply.github.com>
2019-08-24 14:57:48 +0200

    Update FightSimulator.user.js

--- FightSimulator.user.js
+++ FightSimulator.user.js
@@ -1,1 +1,13 @@
+    
+// ==UserScript==
+// @name         FightSimulator
+// @version      0.1
+// @description  Dodatek do gry Pokelife - symulator walki
+// @match        https://gra.pokelife.pl/*
+// @downloadURL  https://github.com/krozum/pokelife/raw/master/FightSimulator.user.js
+// @updateURL    https://github.com/krozum/pokelife/raw/master/FightSimulator.user.js
+// ==/UserScript==
 
+
+var dmg = 1;
+console.log(dmg);
