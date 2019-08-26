faf67f668d383b149def0b4b85f3b244d02230c1
krozum <34769139+krozum@users.noreply.github.com>
2019-08-26 19:12:22 +0200

    0.1.3

--- FightSimulator.user.js
+++ FightSimulator.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         FightSimulator
-// @version      0.1.2
+// @version      0.1.3
 // @description  Dodatek do gry Pokelife - symulator walki
 // @match        https://gra.pokelife.pl/*
 // @downloadURL  https://github.com/krozum/pokelife/raw/master/FightSimulator.user.js
@@ -22,8 +22,6 @@ console.log("min: " + dmg);
 
 dmg =  ((((poziom_pokemona * 0.4 * krytyczny_cios) + 2) * punkty_ataku_pokemona * stała_ataku / 50 / punkty_obrony_pokemona ) + 2) * modyfikator * stab * (losowa_liczba / 255) * 1.5 * 5;
 console.log("random: " +dmg);
-
-var celnosc = (punkty_szybkosci_pokemon / punkty_szybkosci_przeciwnika);
 
 
 var Pokemon = function(id, poziom_pokemona, nazwa, atak, sp_atak, obrona, sp_obrona, szybkosc, zycie, atak1, atak2, atak3, atak4){
@@ -86,6 +84,12 @@ var Pokemon = function(id, poziom_pokemona, nazwa, atak, sp_atak, obrona, sp_obr
             case "Sludge Bomb":
                 useSludgeBomb(root, przeciwnik);
                 break;
+            case "Giga Drain":
+                useGigaDrain(root, przeciwnik);
+                break;
+            case "Synthesis":
+                useSynthesis(root, przeciwnik);
+                break;
             default:
                 console.log('Not working yet');
                 break;
@@ -147,9 +151,11 @@ var Pokemon = function(id, poziom_pokemona, nazwa, atak, sp_atak, obrona, sp_obr
     }
 
     this.setZycie = function(zycie){
+        //console.log(_zycie);
         if(_zycie !== 0){
             _zycie = zycie;
         }
+        //console.log(_zycie);
     };
 
     this.getZycie = function(){
@@ -177,33 +183,104 @@ var Pokemon = function(id, poziom_pokemona, nazwa, atak, sp_atak, obrona, sp_obr
 
 
 
-var pok = new Pokemon(3, 100, "Venek 1", 600, 600, 600, 600, 600, 600, "Leech Seed", "Leech Seed", "Leech Seed", "Leech Seed");
-var pok2 = new Pokemon(4, 100, "Venek 2", 600, 600, 600, 600, 600, 600, "Sludge Bomb", "Sludge Bomb", "Sludge Bomb", "Sludge Bomb");
+var pok = new Pokemon(3, 100, "Venek 1", 600, 1600, 600, 600, 600, 4600, "Leech Seed", "Sludge Bomb", "Giga Drain", "Synthesis");
+var pok2 = new Pokemon(4, 100, "Venek 2", 600, 1600, 600, 600, 601, 4600, "Leech Seed", "Sludge Bomb", "Giga Drain", "Synthesis");
 
 
 
 
 function useLeechSeed(pokemon1, pokemon2){
-    pokemon2.setIsDamagedByLeechSeed(true);
-    console.log(pokemon1.getNazwa() + " uzywa leech seed. Nasiona przyklejają sie");
+    var moc = 0;
+    var celnosc = 90;
+    var typ = "Statusowy";
+
+    if(sprawdzCzyTrafil(celnosc, pokemon1.getSzybkosc(), pokemon2.getSzybkosc())){
+        pokemon2.setIsDamagedByLeechSeed(true);
+        console.log(pokemon1.getNazwa() + " używa ataku Leech Seed, nasiona przyklejają się do przeciwnika.");
+    } else {
+        console.log(pokemon1.getNazwa() + " używa ataku Leech Seed, jednak pudłuje");
+    }
 }
 
 
 function useSludgeBomb(pokemon1, pokemon2){
     var moc = 90;
-    var modyfikator = 1;
+    var celnosc = 100;
+    var typ = "Specjalny";
+
     var stab = 1;
+    var modyfikator = 1;
     var krytyczny_cios = 1;
     var losowa_liczba = Math.floor((Math.random() * 50) + 205);
 
-    var damage = ((((pokemon1.getPoziom() * 0.4 * krytyczny_cios) + 2) * pokemon1.getSpAtak() * moc / 50 / pokemon2.getSpObrona()) + 2) * modyfikator * stab * (losowa_liczba / 255) * 5;
-    if(damage > pokemon2.getZycie()){
-        damage = pokemon2.getZycie();
+    if(sprawdzCzyTrafil(celnosc, pokemon1.getSzybkosc(), pokemon2.getSzybkosc()), typ){
+        var damage = ((((pokemon1.getPoziom() * 0.4 * krytyczny_cios) + 2) * pokemon1.getSpAtak() * moc / 50 / pokemon2.getSpObrona()) + 2) * modyfikator * stab * (losowa_liczba / 255) * 5;
+        if(damage > pokemon2.getZycie()){
+            damage = pokemon2.getZycie();
+        }
+        damage = damage.toFixed(0);
+        pokemon2.setZycie(Number(pokemon2.getZycie()) - damage);
+        console.log(pokemon1.getNazwa() + " używa ataku Sludge Bomb i zadaje " + damage + " obrażeń przeciwnikowi.");
+    } else {
+        console.log(pokemon1.getNazwa() + " używa ataku Sludge Bomb, jednak pudłuje. ");
     }
-    damage = damage.toFixed(0);
-    pokemon2.setZycie(pokemon2.getZycie() - damage);
-    console.log(pokemon1.getNazwa() + " uzywam sludge bomb. Zadaje " + damage + " obrazen");
 }
+
+
+function useGigaDrain(pokemon1, pokemon2){
+    var moc = 75;
+    var celnosc = 100;
+    var typ = "Specjalny";
+
+    var stab = 1;
+    var modyfikator = 1;
+    var krytyczny_cios = 1;
+    var losowa_liczba = Math.floor((Math.random() * 50) + 205);
+
+    if(sprawdzCzyTrafil(celnosc, pokemon1.getSzybkosc(), pokemon2.getSzybkosc()), typ){
+        var damage = ((((pokemon1.getPoziom() * 0.4 * krytyczny_cios) + 2) * pokemon1.getSpAtak() * moc / 50 / pokemon2.getSpObrona()) + 2) * modyfikator * stab * (losowa_liczba / 255) * 5;
+        if(damage > pokemon2.getZycie()){
+            damage = pokemon2.getZycie();
+        }
+        damage = damage.toFixed(0);
+        pokemon2.setZycie(pokemon2.getZycie() - damage);
+        var leczenie = damage * 0.4;
+
+        if(pokemon1.getMaxHpInFight() < pokemon1.getZycie() + leczenie){
+            leczenie = pokemon1.getMaxHpInFight() - pokemon1.getZycie();
+        }
+        leczenie = leczenie.toFixed(0);
+        pokemon1.setZycie(Number(pokemon1.getZycie()) + Number(leczenie));
+        console.log(pokemon1.getNazwa() + " używa ataku Giga Drain i zadaje " + damage + " obrażeń przeciwnikowi. Pokemon leczy się za " + leczenie);
+    } else {
+        console.log(pokemon1.getNazwa() + " używa ataku Giga Drain, jednak pudłuje. ");
+    }
+}
+
+
+function useSynthesis(pokemon1, pokemon2){
+    var moc = 0;
+    var celnosc = 100;
+    var typ = "Statusowy";
+
+    var stab = 1;
+    var modyfikator = 1;
+    var krytyczny_cios = 1;
+    var losowa_liczba = Math.floor((Math.random() * 50) + 205);
+
+    if(sprawdzCzyTrafil(celnosc, pokemon1.getSzybkosc(), pokemon2.getSzybkosc()), typ){
+        var leczenie = pokemon1.getZycie() * 0.25;
+        if(pokemon1.getMaxHpInFight() < pokemon1.getZycie() + leczenie){
+            leczenie = pokemon1.getMaxHpInFight() - pokemon1.getZycie();
+        }
+        leczenie = leczenie.toFixed(0);
+        pokemon1.setZycie(Number(pokemon1.getZycie()) + Number(leczenie));
+        console.log(pokemon1.getNazwa() + " używa ataku Synthesis  i odzyskuje  " + leczenie + "  życia.");
+    } else {
+        console.log(pokemon1.getNazwa() + " używa ataku Synthesis, jednak pudłuje. ");
+    }
+}
+
 
 
 
@@ -219,7 +296,7 @@ function runda(pokemon1, pokemon2){
         if(pokemon1.getZycie() == 0){
             console.log("************************");
             console.log("**                    **");
-            console.log("**  Pokemon 1 wygrał  **");
+            console.log("**  Pokemon 2 wygrał  **");
             console.log("**                    **");
             console.log("************************");
             return;
@@ -227,7 +304,7 @@ function runda(pokemon1, pokemon2){
         if(pokemon2.getZycie() == 0){
             console.log("************************");
             console.log("**                    **");
-            console.log("**  Pokemon 2 wygrał  **");
+            console.log("**  Pokemon 1 wygrał  **");
             console.log("**                    **");
             console.log("************************");
             return;
@@ -244,33 +321,93 @@ function runda(pokemon1, pokemon2){
         console.log("-------------");
         if(pokemon1.getSzybkosc() >= pokemon2.getSzybkosc()){
             pokemon1.atak(pokemon2);
+            if(pokemon1.getZycie() == 0 || pokemon2.getZycie() == 0){
+                return;
+            }
             pokemon2.atak(pokemon1);
+            if(pokemon1.getZycie() == 0 || pokemon2.getZycie() == 0){
+                return;
+            }
         } else {
             pokemon2.atak(pokemon1);
+            if(pokemon1.getZycie() == 0 || pokemon2.getZycie() == 0){
+                return;
+            }
             pokemon1.atak(pokemon2);
+            if(pokemon1.getZycie() == 0 || pokemon2.getZycie() == 0){
+                return;
+            }
         }
         if(pokemon1.getIsDamagedByLeechSeed()){
             var dmg1 = pokemon1.getZycie() * 4/100;
             dmg1 = Number(dmg1.toFixed(0));
-            console.log("Nasiona: "+ dmg1 + " obrazen");
             pokemon1.setZycie(pokemon1.getZycie() - dmg1);
             if(pokemon2.getZycie() + dmg1 > pokemon2.getMaxHpInFight()){
                 pokemon2.setZycie(pokemon2.getMaxHpInFight());
             } else {
                 pokemon2.setZycie(pokemon2.getZycie() + dmg1);
             }
+            console.log(pokemon1.getNazwa() + " traci " + Number(dmg1) + " przez przyklejone nasiona. " + pokemon2.getNazwa() + " leczy się za "+ Number(dmg1) +" życia");
+            if(pokemon1.getZycie() == 0 || pokemon2.getZycie() == 0){
+                return;
+            }
         }
         if(pokemon2.getIsDamagedByLeechSeed()){
             var dmg2 = pokemon2.getZycie() * 4/100;
             dmg2 = Number(dmg2.toFixed(0));
-            console.log("Nasiona: "+ dmg2 + " obrazen");
             pokemon2.setZycie(pokemon2.getZycie() - dmg2);
             if(pokemon1.getZycie() + dmg2 > pokemon1.getMaxHpInFight()){
                 pokemon1.setZycie(pokemon1.getMaxHpInFight());
             } else {
                 pokemon1.setZycie(pokemon1.getZycie() + dmg2);
             }
+            console.log(pokemon2.getNazwa() + " traci " + Number(dmg2) + " przez przyklejone nasiona. " + pokemon1.getNazwa() + " leczy się za "+ Number(dmg2) +" życia");
+            if(pokemon1.getZycie() == 0 || pokemon2.getZycie() == 0){
+                return;
+            }
         }
+    }
+}
+
+
+function sprawdzCzyTrafil(celnosc, szybkosc1, szybkosc2, typ){
+    var zmienna = szybkosc1/szybkosc2;
+    var modified_celnosc;
+
+    if(zmienna >= 3){
+        modified_celnosc = celnosc * 1.4;
+    } else if(zmienna >= 2){
+        modified_celnosc = celnosc * 1.26;
+    } else if(zmienna >= 1.1){
+        modified_celnosc = celnosc * 1.04;
+    } else if(zmienna < 0.33){
+        modified_celnosc = celnosc * 0.6;
+    } else if(zmienna < 0.5){
+        modified_celnosc = celnosc * 0.74;
+    } else if(zmienna < 0.9){
+        modified_celnosc = celnosc * 0.96;
+    } else {
+        modified_celnosc = celnosc;
+    }
+
+    if(modified_celnosc < 0.4 * celnosc){
+        modified_celnosc = 0.4 * celnosc;
+    }
+    if(modified_celnosc > 2 * celnosc){
+        modified_celnosc = 2 * celnosc;
+    }
+    if(typ === "Statusowy" && modified_celnosc > 75){
+        modified_celnosc = 75;
+    }
+
+    var losuj = Math.floor(Math.random() * 101);
+
+    //console.log("modified_celnosc: " + modified_celnosc);
+    //console.log("wylosowano: " + losuj);
+    if(losuj <= modified_celnosc){
+        return true;
+    } else {
+        return false;
     }
 }
 
