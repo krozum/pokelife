b306ec95db61841233f4766cc33d7342ef05499b
krozum <34769139+krozum@users.noreply.github.com>
2020-06-16 08:25:44 +0200

    5.21

--- PokeLifeScript.user.js
+++ PokeLifeScript.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         PokeLifeScript: AntyBan Edition
-// @version      5.20.9
+// @version      5.21
 // @description  Dodatek do gry Pokelife
 // @match        https://gra.pokelife.pl/*
 // @downloadURL  https://github.com/krozum/pokelife/raw/master/PokeLifeScript.user.js
